1ff5fcca62603488f5b8611376b900498d70d64f
mitchellnel <mitchellmclin@gmail.com>
2022-07-21 00:50:48 +0800

    Implement posts query

--- x/blockchainblognel/keeper/grpc_query_posts.go
+++ x/blockchainblognel/keeper/grpc_query_posts.go
@@ -4,20 +4,54 @@ import (
 	"context"
 
 	"blockchain-blog-nel/x/blockchainblognel/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (k Keeper) Posts(goCtx context.Context, req *types.QueryPostsRequest) (*types.QueryPostsResponse, error) {
+func (k Keeper) Posts(
+	goCtx context.Context,
+	req *types.QueryPostsRequest,
+) (*types.QueryPostsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	// get context
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	// define a variable that will store a list of Posts
+	var posts []*types.Post
 
-	return &types.QueryPostsResponse{}, nil
+	// get the key-value module store using the store key (in this case it's "chain")
+	store := ctx.KVStore(k.storeKey)
+
+	// get the part of the store that stores keeps Posts using the PostKey (in this case it's
+	// "Post-value-")
+	postsStore := prefix.NewStore(store, []byte(types.PostKey))
+
+	// paginate the postsStore based on PageRequest
+	pageRes, err := query.Paginate(
+		postsStore,
+		req.Pagination,
+		func(key []byte, value []byte) error {
+			var post types.Post
+			if err := k.cdc.Unmarshal(value, &post); err != nil {
+				return err
+			}
+
+			posts = append(posts, &post)
+			return nil
+		},
+	)
+	if err != nil {
+		// throw an error if pagination failed
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	// return a struct containing a list of posts and pagination info
+	return &types.QueryPostsResponse{Post: posts, Pagination: pageRes}, nil
 }
--- x/blockchainblognel/module.go
+++ x/blockchainblognel/module.go
@@ -1,8 +1,11 @@
 package blockchainblognel
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+
 	// this line is used by starport scaffolding # 1
 
 	"github.com/gorilla/mux"
@@ -14,6 +17,7 @@ import (
 	"blockchain-blog-nel/x/blockchainblognel/client/cli"
 	"blockchain-blog-nel/x/blockchainblognel/keeper"
 	"blockchain-blog-nel/x/blockchainblognel/types"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
@@ -63,7 +67,11 @@ func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
 }
 
 // ValidateGenesis performs genesis state validation for the capability module.
-func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
+func (AppModuleBasic) ValidateGenesis(
+	cdc codec.JSONCodec,
+	config client.TxEncodingConfig,
+	bz json.RawMessage,
+) error {
 	var genState types.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &genState); err != nil {
 		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
@@ -78,6 +86,14 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	// this line is used by starport scaffolding # 2
+	err := types.RegisterQueryHandlerClient(
+		context.Background(),
+		mux,
+		types.NewQueryClient(clientCtx),
+	)
+	if err != nil {
+		log.Printf("Error registers the HTTP handlers for service Query to chain: %v", err)
+	}
 }
 
 // GetTxCmd returns the capability module's root tx command.
@@ -146,7 +162,11 @@ func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
 // InitGenesis performs the capability module's genesis initialization It returns
 // no validator updates.
-func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) []abci.ValidatorUpdate {
+func (am AppModule) InitGenesis(
+	ctx sdk.Context,
+	cdc codec.JSONCodec,
+	gs json.RawMessage,
+) []abci.ValidatorUpdate {
 	var genState types.GenesisState
 	// Initialize global index to index in genesis state
 	cdc.MustUnmarshalJSON(gs, &genState)
